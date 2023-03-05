6d7f4cb3f3136080553962650c051b92798098e5
VinylKittyOasis <killallwalrus@gmail.com>
2023-03-05 15:41:47 +0300

    Start commit

--- Projects/Barbershop/Barber.rb
+++ Projects/Barbershop/Barber.rb
@@ -3,3 +3,20 @@ require 'sinatra'
 get '/' do
         erb :index
     end
+
+post '/' do
+        @user_name =    params[:user_name]
+        @phone_number = params[:phone_number]
+        @date_time =    params[:date_time]
+        
+        @title = 'Thank you!'
+        @message = "Dear #{@user_name}, we`ll be waiting you at #{@date_time}"
+
+        f = File.open 'Users.txt', 'a'
+        f.write "/n User: #{@user_name}, Phone: #{@phone_number}, Date and time: #{@date_time} /n"
+        f.close
+
+        erb :message
+    end     
+
+    #xss
